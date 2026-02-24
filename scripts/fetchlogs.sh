a9e178912c25c0002d1888ee133f8c2874fae55a
Ted <kc8nod@gmail.com>
2026-02-24 14:26:31 -0500

    log fetching script

---
+++ scripts/fetchlogs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+while true; do 
+    hostlist='10.33.02.2 172.22.11.2 roborio-3302-FRC.local'
+    for host in $hostlist; do
+        rsync -av lvuser@$host:/home/lvuser/logs/ :/u/logs ~/Documents/3302/logfiles/ 
+    done
+    sleep 60 
+done
